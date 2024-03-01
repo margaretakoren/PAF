286c5906507e9a19ab4226f0bd5b9604b49d4691
margaretakoren <94902276+margaretakoren@users.noreply.github.com>
2024-03-01 16:07:51 +0100

    Create zadatak3.py

---
+++ Vjezbe/Vjezbe_3/zadatak3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Ulazne točke
+tocke = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
+
+# Izračun i ispis rezultata
+print("Ulazne točke:", tocke)
+print("Aritmetička sredina:", np.mean(tocke))
+print("Standardna devijacija:", np.std(tocke))
