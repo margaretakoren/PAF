3255586fb9dab55589fb8a2417b6bdeadbdca9e0
margaretakoren <94902276+margaretakoren@users.noreply.github.com>
2024-03-08 00:35:06 +0100

    Update particle.py

--- Vjezbe/Vjezbe_4/particle.py
+++ Vjezbe/Vjezbe_4/particle.py
@@ -1,37 +1,53 @@
-import matplotlib.pyplot as plt
 import math
+import matplotlib.pyplot as plt
 
 class Particle:
-    def initialize(tm):
-        tm.x = []
-        tm.y = []
-        tm.t = []
-        tm.vx = []
-        tm.vy = []
+    g = -9.81
 
-     #dt koliki treba biti?
-    def pocetna(tm, v, theta, x_0, y_0, dt):
-        tm.t.append(0)
-        tm.x.append(x_0)
-        tm.y.append(y_0)
-        tm.v_x.append(v*math.cos(math.radians(theta)))
-        tm.v_y.append(v*math.sin(math.radians(theta)))
-        tm.dt = dt
-        tm.v = v
-        tm.theta = theta
+    def __init__(self):
+        self.t, self.x, self.y, self.vx, self.vy, self.ax, self.ay = [], [], [], [], [], [], []
 
-    
-    def reset(tm):
-        tm.x.clear()
-        tm.y.clear()
-        tm.t.clear()
-        tm.vx.clear()
-        tm.vy.clear()
+    def set_initial_conditions(self, v, theta, x0, y0, dt=0.001):
+        self.t.append(0)
+        self.x.append(x0)
+        self.y.append(y0)
+        self.vx.append(v * math.cos(math.radians(theta)))
+        self.vy.append(v * math.sin(math.radians(theta)))
+        self.ax.append(0)
+        self.ay.append(self.g)
+        self.dt = dt
+        self.v = v
+        self.theta = theta
+        
 
-    def __move(tm):
-        tm.t.append(tm.t[-1] + tm.dt)
-        tm.x.append(tm.x[-1] + tm.v_x[-1] * tm.dt)
-        tm.y.append(tm.y[-1] + tm.v_y[-1] * tm.dt)
+    def reset(self):
+        self.t, self.x, self.y, self.vx, self.vy, self.ax, self.ay = [], [], [], [], [], [], []
+        self.dt, self.v, self.theta, self.name = None, None, None, None
 
 
-   
+    def __move(self):
+        self.t.append(self.t[-1] + self.dt)
+        self.ax.append(0)
+        self.ay.append(self.g)
+        self.vx.append(self.vx[-1] + self.ax[-1] * self.dt)
+        self.vy.append(self.vy[-1] + self.ay[-1] * self.dt)
+        self.x.append(self.x[-1] + self.vx[-1] * self.dt)
+        self.y.append(self.y[-1] + self.vy[-1] * self.dt)
+
+    def range(self):
+        while self.y[-1] >= 0:
+            self.__move()
+        return self.x[-1]
+
+    def analytical_range(self):
+        return (self.v ** 2 / abs(self.g)) * math.sin(2 * math.radians(self.theta))
+
+    def plot_trajectory(self):
+        fig = plt.figure(figsize=(20, 10))
+        plt.plot(self.x, self.y, color='magenta', linewidth=2.7)
+        plt.xlabel('x [m]')
+        plt.ylabel('y [m]')
+        plt.title('X-Y graf', fontsize=16, fontweight='bold')  
+        plt.grid(True)  
+        plt.show()
+
