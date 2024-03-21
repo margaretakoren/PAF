c69696a1b630ce576b614722f9c9acd99858082c
margaretakoren <94902276+margaretakoren@users.noreply.github.com>
2024-03-21 19:09:10 +0100

    Create zadatak1.py

---
+++ Vjezbe/Vjezbe_7/zadatak1.py
@@ -0,0 +1,21 @@
+import projectile as pr
+import matplotlib.pyplot as plt
+
+p = pr.Projectile()
+
+dt_values = [0.1, 0.01, 0.001]
+labels = [ "dt=0.1", "dt=0.01","dt=0.001"]
+colors = ['#FF69B4', '#800080', '#4169E1']
+
+fig = plt.figure(figsize=(20, 10), facecolor='#F0F0F0')  
+
+for dt, label, color in zip(dt_values, labels, colors):
+    p.init(10, 45, 0.1, 0.1, 0.05, dt)
+    x, y = p.evolve()
+    plt.plot(x, y, label=label, color=color)
+
+plt.title('Kosi hitac u dvije dimenzije s otporom zraka', fontweight='bold', fontsize = 16)
+plt.xlabel('x [m]')
+plt.ylabel('y [m]')
+plt.legend(loc="upper right")
+plt.show()
