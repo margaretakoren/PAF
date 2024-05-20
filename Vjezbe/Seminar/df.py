f3508ce8b6917234b1a2e6a7d71475bcdaeff405
margaretakoren <94902276+margaretakoren@users.noreply.github.com>
2024-05-20 17:55:51 +0200

    Update df.py

--- Vjezbe/Seminar/df.py
+++ Vjezbe/Seminar/df.py
@@ -6,8 +6,8 @@ pendulum = DoublePendulum()
 t_values, solution = pendulum.solve(dt=0.01)
 
 theta1 = solution[:, 0]
-theta2 = solution[:, 2]
 omega1 = solution[:, 1]
+theta2 = solution[:, 2]
 omega2 = solution[:, 3]
 
 x1 = pendulum.L1 * np.sin(theta1)
@@ -15,17 +15,9 @@ y1 = pendulum.L1 * np.cos(theta1)
 x2 = x1 + pendulum.L2 * np.sin(theta2)
 y2 = y1 + pendulum.L2 * np.cos(theta2)
 
-data = {
-    'time': t_values,
-    'theta1': theta1,
-    'omega1': omega1,
-    'theta2': theta2,
-    'omega2': omega2,
-    'x1': x1,
-    'y1': y1,
-    'x2': x2,
-    'y2': y2
-}
-df = pd.DataFrame(data)
-df.to_csv('double_pendulum_data.csv', index=False)
+data = {'t': t_values,'theta1': theta1,'omega1': omega1,'theta2': theta2,'omega2': omega2,'x1': x1,'y1': y1,'x2': x2,'y2': y2}
 
+df = pd.DataFrame(data)
+
+df.to_csv('double_pendulum_data.csv', index=False)
+print("Spremljeno u double_pendulum_data.csv")
